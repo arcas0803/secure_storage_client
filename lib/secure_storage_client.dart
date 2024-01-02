4edec5f3a35e3529c0dc9d75b5c28fcff0b86966
Alvaro <arcas0803@gmail.com>
2024-01-02 19:54:56 +0100

    Fix imports

--- lib/secure_storage_client.dart
+++ lib/secure_storage_client.dart
@@ -1,5 +1,6 @@
 library secure_storage_client;
 
 export 'src/secure_storage_client.dart';
+export 'src/secure_storage_client_impl.dart';
 export 'src/secure_storage_failure.dart';
 export 'src/secure_storage_failure_util.dart';
